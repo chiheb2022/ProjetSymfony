ea2733faa83f0e42058032dc19179e28dee50fc7
chiheb <chiheb.menjli1920@gmail.com>
2023-03-08 00:47:07 +0100

    maj sur don

--- src/Controller/DonController.php
+++ src/Controller/DonController.php
@@ -8,11 +8,14 @@ use App\Repository\DonRepository;
 use App\Repository\UserRepository;
 use App\Service\EmailService;
 
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
-
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 
 #[Route('/don')]
 class DonController extends AbstractController
@@ -27,18 +30,40 @@ class DonController extends AbstractController
     }
 
     #[Route('/', name: 'app_don_index', methods: ['GET'])]
-    public function index(DonRepository $donRepository ,userRepository $userRepository): Response
+    public function index(DonRepository $donRepository ,userRepository $userRepository , Request $request , PaginatorInterface $paginator): Response
     {
 
-       $iduser = 3 ;
+       $id_role = 2;
+     // $id_role =  $this->getUser()->getRoles()[3] ;
+
+        if ($id_role === 3) {
+            $dons = $donRepository->findAll();
+            $user = $userRepository-> find($id_role);
+        } else {
+            $dons = $donRepository->findBy(['iduserdon' => $id_role]);
+            $user = $userRepository-> find($id_role);
+        }
+
+
+        $pagination = $paginator->paginate(
+            $dons,
+            $request->query->getInt('page', 1),
+            10
+        );
+
         return $this->render('don/index.html.twig', [
-            'dons' => $donRepository->findAll(),
-            'User' => $userRepository-> find($iduser)
+          //  'dons' => $donRepository->findAll(),
+          //  'User' => $userRepository-> find($iduser),
+            'dons' => $pagination ,
+            'User' => $user ,
+            'action' => $id_role ,
         ]);
     }
 
+
+
     #[Route('/new', name: 'app_don_new', methods: ['GET', 'POST'])]
-    public function new(Request $request, DonRepository $donRepository): Response
+    public function new(Request $request, DonRepository $donRepository, MailerInterface $mailer, SessionInterface $session): Response
     {
         $don = new Don();
         $form = $this->createForm(DonType::class, $don);
@@ -46,7 +71,16 @@ class DonController extends AbstractController
 
         if ($form->isSubmitted() && $form->isValid()) {
             $donRepository->save($don, true);
-            $this->emailService->sendEmail('chiheb.menjli1920@gmail.com', 'Nouvelle donation', 'Une nouvelle donation a été ajoutée.');
+
+            $message = (new Email())
+                ->from('chiheb.menjli1920@esprit.tn')
+                ->to('chiheb.menjli1920@esprit.tn')
+                ->subject('Nouvelle donation')
+                ->text('Une nouvelle donation a été ajoutée.');
+
+            $mailer->send($message);
+
+            $session->getFlashBag()->add('success', 'Donation ajoutée avec succès!');
 
             return $this->redirectToRoute('app_don_index', [], Response::HTTP_SEE_OTHER);
         }
@@ -54,11 +88,10 @@ class DonController extends AbstractController
         return $this->renderForm('don/new.html.twig', [
             'don' => $don,
             'form' => $form,
-
         ]);
-
-
     }
+
+
 
     #[Route('/{id}', name: 'app_don_show', methods: ['GET'])]
     public function show(Don $don): Response
